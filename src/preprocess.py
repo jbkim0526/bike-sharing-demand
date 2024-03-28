5e80405bef53be754e5d79633a2470db25f27a88
shyunyy01 <165003061+shyunyy01@users.noreply.github.com>
2024-03-28 06:09:50 +0000

    final

---
+++ src/preprocess.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+from category_encoders import TargetEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import FunctionTransformer
+
+CAT_FEATURES = [
+    "season",
+    "holiday",
+    "workingday",
+    "weather"
+]
+
+
+# 전처리 파이프라인 작성
+# 1. 범주형 변수(CAT_FEATURES)는 타겟 인코딩 적용 (from category_encoders import TargetEncoder)
+preprocess_pipeline = ColumnTransformer(
+    transformers=[
+        (  # 1. 범주형 변수(CAT_FEATURES)는 타겟 인코딩 적용
+            "sqrt_transformer",
+            FunctionTransformer(np.sqrt),
+            ["size"],
+        )
+    ],
+    remainder="passthrough",
+    verbose_feature_names_out=False,
+)
+preprocess_pipeline.set_output(
+    transform="pandas"
+)  
